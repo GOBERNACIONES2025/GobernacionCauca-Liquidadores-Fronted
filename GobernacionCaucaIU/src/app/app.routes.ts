793a27563d5a9d9f131ed16cca5775ff52f4e0da
SamDiper <samueldiazp.dev@gmail.com>
2026-08-10 15:35:18 -0500

    feat: implement automotores module layout, routing, and main application structure

--- GobernacionCaucaIU/src/app/app.routes.ts
+++ GobernacionCaucaIU/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { AutomotoresLayout } from '../features/automotores/presentation/layout/automotores-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'automotores',
+    pathMatch: 'full',
+  },
+  {
+    path: 'automotores',
+    component: AutomotoresLayout,
+    children: [
+      {
+        path: '',
+        redirectTo: 'vehiculos',
+        pathMatch: 'full',
+      },
+      {
+        path: '**',
+        redirectTo: 'vehiculos',
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'automotores',
+  },
+];
