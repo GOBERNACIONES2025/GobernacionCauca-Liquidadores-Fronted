38de3dedd2008dc49a14c691a227dd174506204e
jogav514 <esteban1gaviria@gmail.com>
2026-08-12 14:13:12 -0500

    ---

--- GobernacionCaucaIU/src/app/app.routes.ts
+++ GobernacionCaucaIU/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AutomotoresIndex } from './features/automotores/presentation/pages/automotores-index/automotores-index';
+import { ContribuyentesIndex } from './features/automotores/presentation/pages/contribuyentes-index/contribuyentes-index';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'automotores-index', pathMatch: 'full' },
     { path: 'automotores-index', component: AutomotoresIndex },
+    { path: 'contribuyentes-index', component: ContribuyentesIndex },
 
 ];
